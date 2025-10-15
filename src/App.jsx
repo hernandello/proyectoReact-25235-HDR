3378ac441d353ab9f79cdf377d7adaeee60eb14c
hernandello <drussohernan@gmail.com>
2025-10-15 08:53:02 -0300

    primer commit del Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Ofertas from './components/Ofertas';
+import Infaltables from './components/Infaltables';
+import Login from './components/Login'; 
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+     <Router>
+      <Header />
+      <Routes>
+        <Route path="/administracion" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/ofertas" element={<Ofertas />} />
+        <Route path="/infaltables" element={<Infaltables />} />
+      </Routes>
+      <Footer/> 
+    </Router>
+  )
+}
+
+export default App
